14090d60b967151110c0c6b11ba05bc57d36db57
thenicholashernandez <thenicholashernandez@gmail.com>
2024-03-07 11:10:52 +0800

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+  def hello
+    render html: "hello, world"
+  end
+
+  def home
+    # Your code for the home action
+  end
+
+  def help
+    # Your code for the home action
+  end
+
+  def about
+    # Your code for the home action
+  end
+
+  def contact
+    # Your code for the home action
+  end
+end
