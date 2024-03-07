6892d30e70dd932cad6484e1e853cc4263422c3e
thenicholashernandez <thenicholashernandez@gmail.com>
2024-03-07 11:29:41 +0800

    Edit route name

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,8 +12,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end  
 
   test "should get help" do
-    # get help_path
-    get helf_path
+    get help_path
+    # get helf_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
