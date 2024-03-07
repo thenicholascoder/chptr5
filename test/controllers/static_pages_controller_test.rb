46d1184519faa79cae578b823268ac132c2eacd7
thenicholashernandez <thenicholashernandez@gmail.com>
2024-03-07 12:15:30 +0800

    zebra

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,7 +13,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get help" do
     # get help_path
-    get helf_path
+    get zebra_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
